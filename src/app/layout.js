bf2fe71c6a026cf8e3536342a30d43e899efc62f
budhyp14-alt <budhyp14@gmail.com>
2026-08-23 23:33:27 +0700

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import '../globals.css'
+import Header from '../components/Header'
+
+export const metadata = {
+  title: 'SMP-SMA Advent Batam',
+  description: 'Mendidik Generasi Cerdas dan Berkarakter Kristus',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
